cc2b2536df1194e00968514ed33c4c11fc45f49c
Ruben Pin <pinchasov.ruben@gmail.com>
2023-07-06 19:08:02 +0200

    [test] sending changes

--- src/exit.c
+++ src/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aapostol <aapostol@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpinchas <rpinchas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 18:14:20 by aapostol          #+#    #+#             */
-/*   Updated: 2023/07/06 19:06:48 by aapostol         ###   ########.fr       */
+/*   Updated: 2023/07/07 15:40:17 by rpinchas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,3 @@ void	exit_function(t_data *data)
 	// free data struct;
 	printf("changing my function to create a conflict");
 }
-sdfklsdkfsdjfs q
