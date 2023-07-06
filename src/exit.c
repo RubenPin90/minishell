0b998eec1f0e4ff59d9cd83c14383368567057d5
Adelaide Apostolova <adelaide.apostolova@gmail.com>
2023-07-06 19:07:16 +0200

    [test] Again

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: aapostol <aapostol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 18:14:20 by aapostol          #+#    #+#             */
-/*   Updated: 2023/07/06 19:04:30 by aapostol         ###   ########.fr       */
+/*   Updated: 2023/07/06 19:06:48 by aapostol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	exit_function(t_data *data)
 {
 	// free data struct;
+	printf("changing my function to create a conflict");
 }
 sdfklsdkfsdjfs q
